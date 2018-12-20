633d94ef6b208d41a3df280d189c2909b977283d
fengliu89 <fengliu89@qq.com>
2018-12-20 19:24:44 +0800

    p

--- src/main/java/com/aaa/examination/dao/teacher/BatchStuDao.java
+++ src/main/java/com/aaa/examination/dao/teacher/BatchStuDao.java
@@ -1,5 +1,10 @@
 package com.aaa.examination.dao.teacher;
 
+import org.apache.ibatis.annotations.Insert;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * className:BatchStuDao
  * discriptoin:
@@ -7,5 +12,18 @@ package com.aaa.examination.dao.teacher;
  * createTime:2018-12-10 15:59
  */
 public interface BatchStuDao {
+
+    /**
+     * ftp上传
+     * @return
+     */
+    @Insert("insert into tbl_addexcel(FILE_ID,FILE_PATH,FILE_NAME) values(file_excel.nextval,#{filePath},#{fileName})")
+    int add(Map map);
+
+    /**
+     * 获取文件列表
+     * @return
+     */
+    List<Map> getFtpList();
 }
 
